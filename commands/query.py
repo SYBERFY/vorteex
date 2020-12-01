35102aa953468e4c3c3d3076f5b226ebf8d03dcb
Adrian Dinis <adriandinis99@gmail.com>
2020-12-01 23:12:29 +0000

    Check config before proceeding with queries

--- commands/query.py
+++ commands/query.py
@@ -1,7 +1,27 @@
+import sys
+
+from .. import config
+
+
 def arguments(sub_parser_creator):
     query = sub_parser_creator.add_parser('query', help='Make a query')
+    query.add_argument('-m', '--mode', help='Query mode')
+    query.add_argument('--available-modes', help='Query mode')
     query.set_defaults(query=True, func=main)
 
 
 def main(args):
-    print('query')
+    api_key = config.get_vorteex_api_key()
+    sources = config.get_all_sources()
+
+    if not api_key:
+        print('You must be authenticated to perform queries')
+        print('Please run: vorteex auth')
+        sys.exit(1)
+
+    if not sources:
+        print('No sources available to query from')
+        print('Please run: vorteex configure')
+        sys.exit(1)
+
+    sys.exit(0)
