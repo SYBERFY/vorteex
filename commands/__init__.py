8443481ea783e23deb0b24510f038b796c1f6ad2
Adrian Dinis <adriandinis99@gmail.com>
2020-12-01 23:08:36 +0000

    Create subcommand for authenticating with Vorteex API

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,7 +1,9 @@
-from .configure import arguments as ConfigureArguments
+from .auth import arguments as AuthArguments
 from .query import arguments as QueryArguments
+from .configure import arguments as ConfigureArguments
 
 
 def build_commands(sub_parser_creator):
-    ConfigureArguments(sub_parser_creator)
+    AuthArguments(sub_parser_creator)
     QueryArguments(sub_parser_creator)
+    ConfigureArguments(sub_parser_creator)
